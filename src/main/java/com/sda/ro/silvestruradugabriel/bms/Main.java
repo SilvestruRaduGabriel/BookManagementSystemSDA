716dc267a46e0e46fca89b89e4cb63ea98a131f3
Silvestru Radu-Gabriel <silvestruradugabriel@gmail.com>
2022-02-02 18:57:14 +0200

    Adding initial project structure

---
+++ src/main/java/com/sda/ro/silvestruradugabriel/bms/Main.java
@@ -0,0 +1,7 @@
+package com.sda.ro.silvestruradugabriel.bms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BookingManagementSystem is starting.");
+    }
+}
