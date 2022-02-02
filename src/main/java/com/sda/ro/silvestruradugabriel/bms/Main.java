c752c47adb7240173a75bcba2e895be0c977da92
Silvestru Radu-Gabriel <silvestruradugabriel@gmail.com>
2022-02-02 20:29:41 +0200

    First part from Hibernate Model.

--- src/main/java/com/sda/ro/silvestruradugabriel/bms/Main.java
+++ src/main/java/com/sda/ro/silvestruradugabriel/bms/Main.java
@@ -1,7 +1,32 @@
 package com.sda.ro.silvestruradugabriel.bms;
 
+import com.sda.ro.silvestruradugabriel.bms.model.Author;
+import com.sda.ro.silvestruradugabriel.bms.model.Book;
+import com.sda.ro.silvestruradugabriel.bms.utils.SessionManager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("BookingManagementSystem is starting.");
+        SessionFactory sessionFactory = SessionManager.getSessionFactory();
+
+        Session session = sessionFactory.openSession(); // am deschis sesiunea
+
+        Author author = new Author();
+        author.setFirstName("Creanga");
+        author.setLastName("Ion");
+        session.save(author);
+
+        Book book = new Book();
+        book.setAuthor(author);
+        book.setTitle("Amintiri din Copilarie");
+        book.setIsbn("123323-bms");
+        book.setDescription("Povesti pentru copii");
+        session.save(book);
+
+        session.close();
+
+        SessionManager.shutDown();
     }
 }
---
+++ src/main/java/com/sda/ro/silvestruradugabriel/bms/utils/SessionManager.java
@@ -0,0 +1,27 @@
+package com.sda.ro.silvestruradugabriel.bms.utils;
+
+import com.sda.ro.silvestruradugabriel.bms.model.Author;
+import com.sda.ro.silvestruradugabriel.bms.model.Book;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager {
+
+    private static final SessionManager INSTANCE = new SessionManager();
+    private SessionManager() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE.getSessionFactory("BMS");
+    }
+
+    public static void shutDown() {
+        INSTANCE.shutdownSessionManager();
+    }
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        configuration.addAnnotatedClass(Author.class);
+        configuration.addAnnotatedClass(Book.class);
+    }
+}
